25934aaab1391bbf2b63f0ec7cddec19adf23569
studerd <ledent.n@gmail.com>
2022-03-06 11:03:02 +0100

    [backend]: init backend

---
+++ backend/src/test/java/be/kauffman/kfm/FrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kauffman.kfm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
