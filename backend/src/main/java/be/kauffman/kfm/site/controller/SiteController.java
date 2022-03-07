4b106a8d165e6e444fd54de55938d406bf5b7ef7
ti-ben <benoit.titeux@protonmail.com>
2022-03-07 10:43:57 +0100

    UPDATE Project

--- backend/src/main/java/be/kauffman/kfm/site/controller/SiteController.java
+++ backend/src/main/java/be/kauffman/kfm/site/controller/SiteController.java
@@ -1,12 +1,16 @@
 package be.kauffman.KFM.site.controller;
 
 import be.kauffman.KFM.common.entity.ApiResponse;
+import be.kauffman.KFM.immatriculation.entity.Immatriculation;
+import be.kauffman.KFM.immatriculation.entity.ImmatriculationUpdatePayload;
 import be.kauffman.KFM.site.entity.Site;
 import be.kauffman.KFM.site.entity.SiteCreatePayload;
 import be.kauffman.KFM.site.entity.SiteUpdatePayload;
 import be.kauffman.KFM.site.repository.SiteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -33,13 +37,25 @@ public class SiteController {
         return new ApiResponse(true,siteRepository.findAll(), null);
     }
 
+    // Read record detail
+    @GetMapping("/detail/{id}")
+    public ApiResponse detail(@PathVariable("id") UUID id){
+        Site fromDb = siteRepository.findById(id).orElse(null);
+        if(fromDb == null){
+            return new ApiResponse(false, null, "api.site.detail.not-found");
+        }
+        return new ApiResponse(true,fromDb, null);
+    }
+
     // Update record
     @PostMapping("/update")
-    public ApiResponse update(@RequestBody SiteUpdatePayload payload){
-        Site newSite = new Site.SiteBuilder()
-                .setSite_id(payload.getSite_id())
-                .build();
-        return new ApiResponse(true, siteRepository.save(newSite), null);
+    public ApiResponse update(@RequestBody SiteUpdatePayload payload) {
+        Site fromDb = siteRepository.findById(payload.getSite_id()).orElse(null);
+        if(fromDb == null){
+            return new ApiResponse(false, null, "api.site.update.not-found");
+        }
+        fromDb.setSite_description(payload.getSite_description());
+        return new ApiResponse(true, siteRepository.save(fromDb), null);
     }
 
     // Delete record
--- backend/src/main/java/be/kauffman/kfm/statut/controller/StatutController.java
+++ backend/src/main/java/be/kauffman/kfm/statut/controller/StatutController.java
@@ -1,12 +1,16 @@
 package be.kauffman.KFM.statut.controller;
 
 import be.kauffman.KFM.common.entity.ApiResponse;
+import be.kauffman.KFM.immatriculation.entity.Immatriculation;
+import be.kauffman.KFM.immatriculation.entity.ImmatriculationUpdatePayload;
 import be.kauffman.KFM.statut.entity.Statut;
 import be.kauffman.KFM.statut.entity.StatutCreatePayload;
 import be.kauffman.KFM.statut.entity.StatutUpdatePayload;
 import be.kauffman.KFM.statut.repository.StatutRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -31,13 +35,26 @@ public class StatutController {
         return new ApiResponse(true,statutRepository.findAll(), null);
     }
 
+    // Read record detail
+    @GetMapping("/detail/{id}")
+    public ApiResponse detail(@PathVariable("id") UUID id){
+        Statut fromDb = statutRepository.findById(id).orElse(null);
+        if(fromDb == null){
+            return new ApiResponse(false, null, "api.statut.detail.not-found");
+        }
+        return new ApiResponse(true,fromDb, null);
+    }
+
     // Update record
     @PostMapping("/update")
-    public ApiResponse update(@RequestBody StatutUpdatePayload payload){
-        Statut newStatut = new Statut.StatutBuilder()
-                .setStatut_id(payload.getStatut_id())
-                .build();
-        return new ApiResponse(true, statutRepository.save(newStatut), null);
+    public ApiResponse update(@RequestBody StatutUpdatePayload payload) {
+        Statut fromDb = statutRepository.findById(payload.getStatut_id()).orElse(null);
+        if(fromDb == null){
+            return new ApiResponse(false, null, "api.statut.update.not-found");
+        }
+        fromDb.setStatut_nom(payload.getStatut_nom());
+        fromDb.setStatut_description(payload.getStatut_description());
+        return new ApiResponse(true, statutRepository.save(fromDb), null);
     }
 
     // Delete record
