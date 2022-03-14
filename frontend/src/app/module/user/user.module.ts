c2ff66659570ebbd5cc4c0de0718d47dbbeadb86
studerd <ledent.n@gmail.com>
2022-03-14 21:22:19 +0100

    [frontend]:init project

--- frontend/src/app/module/user/user.module.ts
+++ frontend/src/app/module/user/user.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserFormComponent } from './component/user-form/user-form.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserFormComponent
+  ],
   imports: [
     CommonModule
   ]
