1520c7afb6d8328ddbf8e17a59cd3dda3b8766d8
ti-ben <benoit.titeux@protonmail.com>
2022-03-16 09:04:09 +0100

    Guard, Home Page Error Handler

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,8 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import {ErrorLandingPageComponent} from "./shared/component/error-landing-page/error-landing-page.component";
 
 const routes: Routes = [
-  //{path: '', loadChildren: ()=> import('./module/public/public.module').then((m)=>m.PublicModule)},
-  {path: 'home', loadChildren: ()=> import('./module/site/site.module').then((m)=>m.SiteModule)},
   {path: '**', component:ErrorLandingPageComponent}
 ];
 
