b023066de4f218aa7ef78e70b63373cb42f6c51a
ti-ben <benoit.titeux@protonmail.com>
2022-03-20 21:37:23 +0100

    Guard, Home Page Error Handler

--- frontend/src/app/module/user/component/user-detail/user-detail.component.ts
+++ frontend/src/app/module/user/component/user-detail/user-detail.component.ts
@@ -1,4 +1,10 @@
 import {Component, OnInit} from '@angular/core';
+import {UserService} from "@user/service/user.service";
+import {BehaviorSubject} from "rxjs";
+import {GenericTableConfig} from "@shared/model";
+import {tap} from "rxjs/operators";
+import {User} from "@user/model";
+import {GenericTableHelper} from "@shared/helper";
 
 @Component({
   selector: 'app-user-detail',
@@ -6,11 +12,21 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./user-detail.component.scss']
 })
 export class UserDetailComponent implements OnInit {
+  config$: BehaviorSubject<GenericTableConfig> = new BehaviorSubject<GenericTableConfig>({data: [], fields: []});
 
-  constructor() {
+  constructor(public userService: UserService) {
   }
 
   ngOnInit(): void {
+    this.userService.list().pipe(
+      tap((list: User[]) => this.setConfig(list)))
+      .subscribe();
   }
 
+  private setConfig(list: User[]): void {
+    let config = this.config$.getValue();
+    config.fields = GenericTableHelper.genUserFieldDefinitions();
+    config.data = list;
+    this.config$.next(config);
+  }
 }
