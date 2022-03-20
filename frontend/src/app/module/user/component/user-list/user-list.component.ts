fc819ff8d2cf387365d861f96329461e0ebf06fc
ti-ben <benoit.titeux@protonmail.com>
2022-03-20 21:06:37 +0100

    Guard, Home Page Error Handler

--- frontend/src/app/module/user/component/user-list/user-list.component.ts
+++ frontend/src/app/module/user/component/user-list/user-list.component.ts
@@ -1,31 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '@user/service/user.service';
-import { BehaviorSubject } from 'rxjs';
-import { User } from '@user/model';
-import { tap } from 'rxjs/operators';
-import { FieldDefinition, FieldType, GenericTableConfig } from '@shared/model';
-import { GenericTableHelper } from '@shared/helper';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '@user/service/user.service';
+import {BehaviorSubject} from 'rxjs';
+import {User} from '@user/model';
+import {tap} from 'rxjs/operators';
+import {GenericTableConfig} from '@shared/model';
+import {GenericTableHelper} from '@shared/helper';
 
 @Component({
-    selector: 'app-user-list',
-    templateUrl: './user-list.component.html',
-    styleUrls: ['./user-list.component.scss']
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
-    config$: BehaviorSubject<GenericTableConfig> = new BehaviorSubject<GenericTableConfig>({data: [], fields: []});
+  config$: BehaviorSubject<GenericTableConfig> = new BehaviorSubject<GenericTableConfig>({data: [], fields: []});
 
-    constructor(public userService: UserService) { }
+  constructor(public userService: UserService) {
+  }
 
-    ngOnInit(): void {
-        this.userService.list().pipe(
-            tap((list: User[]) => this.setConfig(list)))
-            .subscribe();
-    }
+  ngOnInit(): void {
+    this.userService.list().pipe(
+      tap((list: User[]) => this.setConfig(list)))
+      .subscribe();
+  }
 
-    private setConfig(list: User[]): void {
-        let config = this.config$.getValue();
-        config.fields = GenericTableHelper.genUserFieldDefinitions();
-        config.data = list;
-        this.config$.next(config);
-    }
+  private setConfig(list: User[]): void {
+    let config = this.config$.getValue();
+    config.fields = GenericTableHelper.genUserFieldDefinitions();
+    config.data = list;
+    this.config$.next(config);
+  }
 }
