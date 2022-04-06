7be186d818c839ec4b2dd060ba6068bc73f60ace
ti-ben <51511947+ti-ben@users.noreply.github.com>
2022-04-06 17:27:37 +0200

    [UPDATE] change UUID format for entity Prestataire.java

--- backend/src/main/java/be/kauffman/kfm/modules/prestataire/entity/dto/Prestataire.java
+++ backend/src/main/java/be/kauffman/kfm/modules/prestataire/entity/dto/Prestataire.java
@@ -15,9 +15,9 @@ import java.util.UUID;
 @Entity
 public class Prestataire {
     @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(name = "prestataire_id", updatable = false, nullable = false)
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
     private UUID prestataire_id;
     String name;
     String tel;
