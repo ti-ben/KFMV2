215f01d9dd7e1e5ce462219dda46de36bdf13134
studerd <ledent.n@gmail.com>
2022-04-24 13:28:20 +0200

    [frontend]:generic form

--- frontend/src/app/module/site/component/site-detail/site-detail.component.ts
+++ frontend/src/app/module/site/component/site-detail/site-detail.component.ts
@@ -1,18 +1,14 @@
-import {Component, Input, OnInit, Output} from '@angular/core';
-import {CardConfig, SelectConfig} from "@shared/model";
-import {tap} from "rxjs/operators";
-import {isNil} from "lodash";
-import {SiteService} from "@site/service/site.service";
-import {Site, SiteUpdatePayload} from "@site/model";
-import {CardHelper} from "@shared/helper/card.helper";
-import {FormControl, FormGroup} from "@angular/forms";
-import {ActivatedRoute, Params, Router} from "@angular/router";
-import {SiteHelper} from "@site/helper";
-import {ActifHelper, DriverHelper, GenderHelper} from "@shared/helper";
-import {Status} from "@status/model";
-import {StatusHelper} from "@status/helper";
-import {Grade} from "@grade/model";
-import {GradeHelper} from "@grade/helper";
+import { Component, Input, OnInit } from '@angular/core';
+import { CardConfig, SelectConfig } from "@shared/model";
+import { tap } from "rxjs/operators";
+import { isNil } from "lodash";
+import { SiteService } from "@site/service/site.service";
+import { Site, SiteUpdatePayload } from "@site/model";
+import { CardHelper } from "@shared/helper/card.helper";
+import { FormControl, FormGroup } from "@angular/forms";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { SiteHelper } from "@site/helper";
+import { ActifHelper } from "@shared/helper";
 
 @Component({
   selector: 'app-site-detail',
@@ -34,12 +30,15 @@ export class SiteDetailComponent implements OnInit {
     return this.formGroup.get(name) as FormControl;
   }
 
-  private initForm(): void {
-    this.formGroup = SiteHelper.toFormGroup(); //A faire
+  private initForm(site: Site): void {
+    this.formGroup = SiteHelper.toFormGroup(site); //A faire
   }
 
   ngOnInit(): void {
-    this.initForm();
+    this.siteService.currentDetail$.subscribe((site: Site) => {
+      this.detail = site;
+      this.initForm(site);
+    })
     this.setSelectConfig();
     this.activatedRouter.params
       .pipe(
@@ -52,17 +51,9 @@ export class SiteDetailComponent implements OnInit {
       ).subscribe();
   }
 
-  ngOnChanges(): void {
-    this.formGroup = new FormGroup({
-      site_id: new FormControl(this.detail.site_id),
-      name: new FormControl(this.detail.name),
-      description: new FormControl(this.detail.description),
-      created_on: new FormControl(this.detail.created_on),
-    });
-  }
-
   //todo update site information to db
   update(): void {
+    console.log('mes valeurs', this.formGroup.value);
     if (this.formGroup.valid) {
       const payload: SiteUpdatePayload = this.formGroup.value;
       payload.site_id = this.detail.site_id;
--- frontend/src/app/shared/component/form/input-text/input-text.component.ts
+++ frontend/src/app/shared/component/form/input-text/input-text.component.ts
@@ -1,21 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { LabelWithParam } from '@shared/model';
 import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-input-text',
-  templateUrl: './input-text.component.html',
-  styleUrls: ['./input-text.component.scss']
+    selector: 'app-input-text',
+    templateUrl: './input-text.component.html',
+    styleUrls: ['./input-text.component.scss']
 })
 export class InputTextComponent implements OnInit {
 
-  @Input() label!: LabelWithParam;
-  @Input() placeholder!: string;
-  @Input() ctrl!: FormControl;
+    @Input() label!: LabelWithParam;
+    @Input() placeholder!: string;
+    @Input() ctrl!: FormControl;
+    @Output() onBlur = new EventEmitter<void>();
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    ngOnChange() {
+        console.log('ca change');
+    }
 
 }
