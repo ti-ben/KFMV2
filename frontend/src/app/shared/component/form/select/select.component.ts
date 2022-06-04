674be40d2fd931e9b99a6cbe5235f7b8beb47ace
ti-ben <benoit.titeux@protonmail.com>
2022-06-04 11:13:00 +0200

    Minor changes

--- frontend/src/app/shared/component/form/select/select.component.ts
+++ frontend/src/app/shared/component/form/select/select.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, SimpleChanges} from '@angular/core';
 import {SelectConfig} from '@shared/model/select.config';
 
 @Component({
